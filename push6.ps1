0fc7afd7f447394b70092f5c7cd5a3fbc7fd7c33
Vũ Hải Quân <vhq.it.2003@gmail.com>
2026-05-20 16:09:02 +0700

    chore: remove temp script

--- push6.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location 'c:\Users\VuHaiQuan\OneDrive\Desktop\test'
-git rm --cached vercel.json 2>$null
-git add -A
-git commit -m "chore: remove vercel.json, use dashboard config only"
-git push origin main
